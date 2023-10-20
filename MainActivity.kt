cb4c70b0a2f7d40a5b4de8bc42eee82a9962c90a
ruthlngser <113416376+ruthlngser@users.noreply.github.com>
2023-10-20 23:45:00 +0700

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -19,6 +19,8 @@ import androidx.compose.ui.unit.sp
 import id.utdi.utsruth215410011.ui.theme.UTSRuth215410011Theme
 import androidx.compose.ui.tooling.preview.Preview
 
+//MainActivity adalah kelas turunan dari `ComponentActivity` yang merupakan komponen inti dalam arsitektur Android. 
+//Fungsi `onCreate` di dalamnya menetapkan antarmuka pengguna utama ke `FlowerCard` dengan menggunakan `setContent` dan menerapkan tema dari `UTSRuth215410011Theme`.
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,7 +68,8 @@ fun FlowerCard() {
 
         ),
     )
-
+    //`LazyColumn` untuk menampilkan daftar elemen secara efisien, yaitu daftar jenis bunga. 
+    // Setiap item di LazyColumn mencakup gambar bunga, nama bunga, deskripsi bunga, dan tombol aksi "Previous" dan "Next".
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
@@ -75,6 +78,7 @@ fun FlowerCard() {
         item {
             // Gambar Bunga
             val imagePainter: Painter = painterResource(id = flowerList[currentFlowerIndex].imageResource)
+            //Menggunakan komponen `Image` untuk menampilkan gambar bunga. Gambar diambil dari resource drawable menggunakan `painterResource`.
             Image(
                 painter = imagePainter,
                 contentDescription = null,
@@ -121,7 +125,9 @@ fun FlowerCard() {
                 }
 
                 Spacer(modifier = Modifier.width(8.dp))
-
+                
+                //Menggunakan tombol `Button` untuk mengaktifkan aksi "Previous" dan "Next". 
+                //Selain itu, `clickable` diterapkan pada gambar bunga sehingga deskripsi bunga dapat ditampilkan atau disembunyikan dengan mengklik gambar.
                 Button(
                     onClick = {
                         // sebagai tombol untuk aksi selanjutnya
@@ -137,6 +143,8 @@ fun FlowerCard() {
     }
 }
 
+//Menggunakan anotasi `@Preview` untuk menampilkan preview antarmuka pengguna (`FlowerCardPreview`) 
+//selama pengembangan dalam lingkungan pengembangan (IDE).
 @Preview(showBackground = true)
 @Composable
 fun FlowerCardPreview() {
@@ -144,3 +152,7 @@ fun FlowerCardPreview() {
         FlowerCard()
     }
 }
+
+//Dengan adanya aplikasi yang memungkinkan pengguna 
+//menjelajahi daftar jenis bunga dengan gambar dan deskripsi yang berubah 
+//sesuai dengan tombol "Previous" dan "Next".
